74fb810776ac4a324675ea5bf885ad5d5fb1c038
shawn <shawn@kali.shawn>
2021-12-13 07:26:40 -0900

    change requirements.txt

--- instagram/settings.py
+++ instagram/settings.py
@@ -131,13 +131,12 @@ cloudinary.config(
 )
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'instagram',
         'USER': 'shawn',
-    'PASSWORD':'qwanza',
+        'PASSWORD':'qwanza',
     }
 }
 
