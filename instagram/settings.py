71333ef6e99e015418a8e9d5b44b6c75cd09a962
shawn <shawn@kali.shawn>
2021-12-13 23:02:25 -0900

    add click

--- instagram/settings.py
+++ instagram/settings.py
@@ -46,7 +46,7 @@ else:
            default=config('DATABASE_URL')
        )
    }
-db_from_env = dj_database_url.config(conn_max_agegit=500)
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
